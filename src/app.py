c955a20301ae9a2d0ceb82f3d2cf8757106510c7
Alvin <alvinlee123@hotmail.com>
2017-01-10 21:02:28 -0800

    User model modified
    HTML files created

--- src/app.py
+++ src/app.py
@@ -1,32 +1,48 @@
-from flask import request
-from flask import session
-
 from src.common.database import Database
 from src.models.user import User
 
 __author__ = 'alee'
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__) # '__main__' private variable
 app.secret_key = 'alvin'
 
-@app.route('/') #www.mysite.com/api/
+@app.route('/') #127.0.0.1:4995
+def home_template():
+    return render_template('home.html')
 
-def hello_method():
+
+@app.route('/login') #127.0.0.1:4995/login
+def login_template():
     return render_template('login.html')
+
+@app.route('/register') #127.0.0.1:4995/register
+def register_template():
+    return render_template('register.html')
+
 
 @app.before_first_request
 def initialize_database():
     Database.initialize()
 
-@app.route('/login', methods=['POST'])
+@app.route('/auth/login', methods=['POST'])
 def login_user():
     email = request.form['email']
     password = request.form['password']
 
     if User.login_valid(email,password):
         User.login(email)
+    else:
+        session['email'] = None
+    return render_template("profile.html", email=session['email'])
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email,password)
 
     return render_template("profile.html", email=session['email'])
 
